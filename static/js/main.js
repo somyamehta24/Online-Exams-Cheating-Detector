a2962a670278828cf92b6a7500ecf85ebcdc7428
= <garvitgalgat@gmail.com>
2020-10-31 03:26:03 +0530

    flask added

---
+++ static/js/main.js
@@ -0,0 +1,44 @@
+// ------------- DETECTING TAB CHANGES---------------------
+
+var warn = 0
+
+$(window).blur(function () {
+    msg = ""
+    warn++;
+    if (warn == 1) {
+
+        msg += "First Warning"
+    }
+    if (warn == 2) {
+
+        msg += "Second Warning"
+    }
+    if (warn == 3) {
+
+        msg += "Last Warning"
+    }
+
+    if (warn > 3) {
+        msg = "Submitted"
+        $(".container").hide()
+
+    }
+
+    alert(msg)
+});
+
+// -----------------------------------------------------------
+
+$(window).on('resize', function () {
+    $(".container").hide()
+    alert("Please do not resize the page")
+});
+
+
+// Disable copy paste
+$(document).ready(function () {
+    $('.form-group').on("cut copy paste", function (e) {
+        e.preventDefault();
+    });
+});
+
